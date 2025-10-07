d8c7807a406524443bd92386d5be7c2c6509c77b
ArmandoBeriX <158400084+ArmandoBeriX@users.noreply.github.com>
2025-10-07 12:23:38 -0800

    Delete src directory

--- src/components/EntityRelationshipDiagram/index.js
+++
@@ -1,4 +0,0 @@
-// src/components/EntityRelationshipDiagram/index.js
-import EntityRelationshipDiagram from './EntityRelationshipDiagram';
-import './EntityRelationshipDiagram.css';
-export default EntityRelationshipDiagram;
