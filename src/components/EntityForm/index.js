39f738bd747ed7e32aac70c52d4f6358da4afdcd
ArmandoBeriX <158400084+ArmandoBeriX@users.noreply.github.com>
2025-10-16 07:05:18 -0800

    Add files via upload

---
+++ src/components/EntityForm/index.js
@@ -0,0 +1,20 @@
+// src/components/EntityRelationshipDiagram/index.js
+import EntityRelationshipDiagram from './EntityRelationshipDiagram';
+import TableNode from './TableNode';
+import FieldNode from './FieldNode';
+import './EntityRelationshipDiagram.css';
+import NavigationBar from './NavigationBar';
+import EntityForm from './EntityForm'; 
+
+
+
+// Exportar todos los componentes para facilitar su uso
+export {
+  TableNode,
+  FieldNode,
+  NavigationBar,
+  EntityForm
+
+};
+
+export default EntityRelationshipDiagram;
